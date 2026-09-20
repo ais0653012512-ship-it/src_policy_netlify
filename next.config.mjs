e2af2eb93f30c631737eca54160c7ca89dabbe2e
Trandung-02 <email_cua_ban@example.com>
2026-09-20 14:32:14 +0700

    20.09

--- next.config.mjs
+++ next.config.mjs
@@ -54,27 +54,6 @@ const nextConfig = {
       },
     ]
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    })
-    return config
-  },
 }
 
 export default nextConfig
